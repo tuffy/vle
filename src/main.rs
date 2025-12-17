cc3ccda9b517a0cd6da320d20784896e5962afc1
Brian Langenberger <bjl@usa.net>
2025-12-17 12:36:43 -0600

    Add preliminary file display

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 fn main() -> std::io::Result<()> {
-    use crossterm::{execute, {cursor::MoveTo, terminal::{Clear, ClearType}}};
-    use std::io::Write;
+    use crossterm::{
+        execute,
+        {
+            cursor::{MoveTo, MoveToNextLine},
+            terminal::{Clear, ClearType, size},
+        },
+    };
+    use std::borrow::Cow;
+    use std::fs::File;
+    use std::io::{BufReader, Write};
+
+    let file = ropey::Rope::from_reader(BufReader::new(File::open(
+        std::env::args_os().skip(1).next().unwrap(),
+    )?))?;
 
     execute_terminal(|stdout| {
-        execute!(
-            stdout,
-            Clear(ClearType::All),
-            MoveTo(0, 0),
-        )?;
+        execute!(stdout, Clear(ClearType::All), MoveTo(0, 0),)?;
 
-        writeln!(stdout, "Hello World!")?;
+        let (_cols, rows) = size()?;
+
+        for line in file.lines().take(rows.into()) {
+            write!(stdout, "{}", Cow::from(line).trim_end())?;
+            execute!(stdout, MoveToNextLine(1))?;
+        }
 
         execute!(stdout, MoveTo(0, 0))?;
 
@@ -20,7 +33,9 @@ fn main() -> std::io::Result<()> {
 }
 
 /// Sets up terminal, executes editor, and automatically cleans up afterward
-fn execute_terminal<T>(f: impl FnOnce(&mut std::io::Stdout) -> std::io::Result<T>) -> std::io::Result<T> {
+fn execute_terminal<T>(
+    f: impl FnOnce(&mut std::io::Stdout) -> std::io::Result<T>,
+) -> std::io::Result<T> {
     use crossterm::{
         execute,
         terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
